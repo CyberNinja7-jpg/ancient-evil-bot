65c4279563bebc3497f9be4ae94494537d4a009e
CyberNinja7-jpg <albertnyasimi27@gmail.com>
2025-08-31 15:38:04 +0300

    Create commands.js

---
+++ src/commands.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "!curse": "☠ Ancient curse unleashed upon your enemies!",
+    "!summon": "🔥 A dark entity has answered your call...",
+    "!doom": "💀 The end is written in blood and fire.",
+    "!darkness": "🌑 Shadows consume the weak.",
+    "!rahl": "👑 Lord Rahl reigns eternal!"
+};
